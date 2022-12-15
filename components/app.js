7431152c5c1bd1ed1d2a085b1837676fcb7e510f
Hoàng Bảo Phú <57654376+hoangbaophu89@users.noreply.github.com>
2022-12-15 10:00:49 +0700

    Initial commit

---
+++ components/app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import iconLibrary from './utils/iconLIbrary'
+import english from './utils/translations/en'
+import Head from 'next/head'
+import Translator from 'simple-translator'
+import { globalStyle, theme } from './utils/theme.config'
+import { ThemeProvider } from 'emotion-theming'
+import { Global } from '@emotion/core'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+const AppLayout = props => {
+  iconLibrary()
+  Translator.registerDefaultLanguage("en", english)
+  return(
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyle}/>
+      <Head>
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
+      </Head>
+      {props.children}
+    </ThemeProvider>
+  )
+}
+
+export default AppLayout
